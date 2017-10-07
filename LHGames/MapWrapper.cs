5c3b5517e15e1a1bfe8e1aca3c7736c129303661
Jean-Christophe Buteau <dgici@hotmail.com>
2017-10-07 12:08:22 -0400

    added mapwrapper

---
+++ LHGames/MapWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using StarterProject.Web.Api;
+
+namespace LHGames
+{
+    public class MapWrapper
+    {
+        public Dictionary<Point, Tile> Map;
+        public List<Point> TraveledPositions;
+
+        public MapWrapper(Point startPosition, Tile[,] map)
+        {
+            CreateMap(map);
+        }
+
+        private void CreateMap(Tile[,] map)
+        {
+            foreach (Tile t in map)
+            {
+                Map.TryAdd(new Point(t.X, t.Y), t);
+            }
+        }
+
+        public void UpdateMap(Tile[,] map)
+        {
+            
+        }
+
+        public List<Tile> ChangedTiles()
+        {
+            return new List<Tile>();
+        }
+
+        private List<Point> TilesToDiscover()
+        {
+            return new List<Point>();
+        }
+    }
+}
