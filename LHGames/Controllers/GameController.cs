c9fa3b3a61fbd596d93b0544478193db59db8b68
Olivier <tipitou1er@hotmail.com>
2017-10-07 16:11:47 -0400

    gheto

--- LHGames/Controllers/GameController.cs
+++ LHGames/Controllers/GameController.cs
@@ -13,22 +13,63 @@
         AIHelper player = new AIHelper();
         MapWrapper mapWrapper = null;
 
+        int nbTurns = 0;
+
         [HttpPost]
         public string Index([FromForm]string map)
         {
             GameInfo gameInfo = JsonConvert.DeserializeObject<GameInfo>(map);
             var carte = AIHelper.DeserializeMap(gameInfo.CustomSerializedMap);
             
-            if (mapWrapper == null) {
-                mapWrapper = new MapWrapper(gameInfo.Player.Position, carte);
-            } else {
-                mapWrapper.UpdateMap(carte, gameInfo.Player.Position);
-            }
+            //if (mapWrapper == null) {
+            //    mapWrapper = new MapWrapper(gameInfo.Player.Position, carte);
+            //} else {
+            //    mapWrapper.UpdateMap(carte, gameInfo.Player.Position);
+            //}
 
-            var path = mapWrapper.Map.FindPath(new Point(15, 17), new Point(15, 21));
-            Console.WriteLine("Count: " + path.Count);
+            //var path = mapWrapper.Map.FindPath(new Point(15, 17), new Point(15, 21));
+            //Console.WriteLine("Count: " + path.Count);
 
             string action = AIHelper.CreateMoveAction(gameInfo.Player.Position);
+
+            string[] actions =
+            {
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, -1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(1, 0)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(1, 0)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateCollectAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(0, 1)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(-1, 0)),
+                AIHelper.CreateMoveAction(gameInfo.Player.Position + new Point(-1, 0)),
+            };
+
+            action = actions[nbTurns];
+
+            nbTurns = (nbTurns + 1) % 30;
+
             return action;
         }
     }
